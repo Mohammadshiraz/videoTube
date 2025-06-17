ccc8295a27b0263f107cc4101c48ef4f70df161e
Mohammad Shiraz <mohdshiraz980@gmail.com>
2025-06-17 14:11:38 +0530

    create utils

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+//cors use to accept frontend request
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//cookie parser accept the cookie from user
+app.use(cookieParser());
+
+//json accept the json format from the user
+app.use(express.json({ limit: "16kb" }));
+
+//urlencoded accept all type of url character
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+
+//static create a folder and store data in server
+app.use(express.static("public"));
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "../src/db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`server started at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed", err);
+  });
